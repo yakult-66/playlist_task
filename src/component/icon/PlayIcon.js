2cd634d4a7f4ec45f25d5665c148831824ec246f
nihei <yakult.jp66@gmail.com>
2021-06-28 00:35:38 +0900

    SVGアイコンの記載方法の変更

--- src/component/icon/PlayIcon.js
+++ src/component/icon/PlayIcon.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { SvgIcon } from "@material-ui/core";
 
-export const PlayIcon = () => {
+export const PlayIcon = (props) => {
   return (
-    <SvgIcon>
+    <SvgIcon {...props}>
       <svg
-        width="17"
+        width="20"
         height="20"
-        viewBox="0 0 17 20"
-        fill="none"
+        viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path
-          d="M15.24 8.59992L2.46 1.21992C1.38 0.579917 0 1.37992 0 2.63992V17.3999C0 18.6599 1.38 19.4599 2.46 18.8199L15.24 11.4399C16.34 10.8199 16.34 9.23992 15.24 8.59992Z"
-          fill="#F9FAFC"
-        />
+        <path d="M15.24 8.59992L2.46 1.21992C1.38 0.579917 0 1.37992 0 2.63992V17.3999C0 18.6599 1.38 19.4599 2.46 18.8199L15.24 11.4399C16.34 10.8199 16.34 9.23992 15.24 8.59992Z" />
       </svg>
     </SvgIcon>
   );
